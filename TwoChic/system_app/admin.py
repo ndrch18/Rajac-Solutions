18a28297264a05d99d042ce03aba227d5bcd0829
Ryan <ryan.pintor@student.ateneo.edu>
2026-03-27 07:22:05 +0800

    Added Products to admin.py

--- TwoChic/system_app/admin.py
+++ TwoChic/system_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models  import RawMaterial, MaterialUnit, Account
+from .models  import RawMaterial, MaterialUnit, Account, Product
 
 admin.site.register(RawMaterial)
 admin.site.register(MaterialUnit)
 admin.site.register(Account)
+admin.site.register(Product)
 
